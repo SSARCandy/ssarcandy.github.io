82997a38386f6cd58893718e007dbab5c93b617a
HSU,SHU-HSUAN <ssarcandy@gmail.com>
2017-09-29 14:46:06 -0500

    Visitor count (#72)
    * registor visitor_count helper
    * add visitors count to footer

--- scripts/helper.js
+++ scripts/helper.js
@@ -2,6 +2,7 @@
 
 var _ = require('lodash');
 var lunr = require('lunr');
+var gaAnalytics = require("ga-analytics");
 
 
 hexo.extend.helper.register('lunr_index', function(data){
@@ -40,3 +41,33 @@ hexo.extend.tag.register('zoom', function(args){
     '<img src="' + (thumbnail || original) + '" alt="' + title + '" data-action="zoom">'+
     '</div>';
 });
+
+
+var visitor_count = '';
+
+var date_formatter = function (d) {
+  var mm = d.getMonth() + 1; // getMonth() is zero-based
+  var dd = d.getDate();
+
+  return [d.getFullYear(), (mm > 9 ? '' : '0') + mm, (dd > 9 ? '' : '0') + dd].join('-');
+};
+
+gaAnalytics({
+  metrics: "ga:users",
+  clientId: process.env.GOOGLEAPI_CLIENTID,
+  serviceEmail: process.env.GOOGLEAPI_EMAIL,
+  key: process.env.GOOGLEAPI_KEY,
+  ids: process.env.GOOGLEAPI_ANALYTICS_TABLE,
+  startDate: "2016-07-20",
+  endDate: date_formatter(new Date()),
+  dimensions: "ga:pagePath",
+  metrics: "ga:pageviews"
+}, function (err, res) {
+  if (err) return;
+  visitor_count = res.totalsForAllResults['ga:pageviews'];
+});
+
+hexo.extend.helper.register('visitor_count', function () {
+  return visitor_count
+});
+
