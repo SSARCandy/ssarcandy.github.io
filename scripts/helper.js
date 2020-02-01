44f3303f32d82ab18b8038a0e74edb0fd96e9eca
ssarcandy <ssarcandy@gmail.com>
2020-02-02 00:49:47 +0800

    remove zoom.js and seperate to independent plugin

--- scripts/helper.js
+++ scripts/helper.js
@@ -60,26 +60,3 @@ hexo.extend.helper.register('lunr_index', data => {
 
   return JSON.stringify(index.toJSON());
 });
-
-
-/**
- * zoom tag
- *
- * Syntax:
- *   {% zoom /path/to/image [/path/to/thumbnail] [title] %}
- */
-const rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
-hexo.extend.tag.register('zoom', (args) => {
-  const original = args.shift();
-  
-  let thumbnail = '';
-  if (args.length && rUrl.test(args[0])) {
-    thumbnail = args.shift();
-  }
-
-  const title = args.join(' ');
-
-  return `<div>
-            <img src="${(thumbnail || original)}" alt="${title}" data-action="zoom">
-          </div>`;
-});
