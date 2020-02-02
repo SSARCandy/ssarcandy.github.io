c7838e1e2a53a0d21be50ec90015c162a64be6e9
bot <ssarcandy@gmail.com>
2020-02-03 02:22:56 +0800

    Site updated: 2020-02-03 02:22:56

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/2019/06/11/Monitoring-from-Handcraft-to-Prometheus/');
 
-  precacheUrls.push('/2019/03/16/Setup-Slurm-Cluster/');
+  precacheUrls.push('/2018/01/15/use-pytorch-to-solve-fizzbuzz/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
