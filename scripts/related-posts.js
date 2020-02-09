66db972d142fbfd196588183e91c16f77ab8f433
ssarcandy <ssarcandy@gmail.com>
2020-02-09 18:31:11 +0800

    fix related post recommand itself

--- scripts/related-posts.js
+++ scripts/related-posts.js
@@ -27,5 +27,6 @@ hexo.extend.helper.register('lunr_related_posts', (data, kw, result_len = 5) => 
   const index = build_index(filtered_data);
   const related_posts = index.search(kw).map(r => filtered_data[r.ref]);
 
-  return JSON.stringify(related_posts.slice(0, result_len));
+  // slice from 1 because first one always is itself.
+  return JSON.stringify(related_posts.slice(1, result_len + 1));
 })
