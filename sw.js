dd2dff6f8a85b4e7d34f41ee0f6abb68af99842b
bot <ssarcandy@gmail.com>
2020-05-11 00:48:53 +0800

    Site updated: 2020-05-11 00:48:52

--- sw.js
+++ sw.js
@@ -24,13 +24,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2016/07/29/hexo-auto-deploy/');
+  precacheUrls.push('/2019/10/02/game-design-from-perspective-of-hacker/');
 
   precacheUrls.push('/2020/04/22/Build-a-High-Performance-Computing-Cluster-on-GCP/');
 
-  precacheUrls.push('/2017/12/06/javascript-prototype-chain/');
+  precacheUrls.push('/2018/08/18/mbpr2015-repair/');
 
-  precacheUrls.push('/2017/04/16/High-Dynamic-Range-Imaging/');
+  precacheUrls.push('/2016/12/25/MSAuto/');
 
   precacheUrls.push('/2019/06/11/Monitoring-from-Handcraft-to-Prometheus/');
 
