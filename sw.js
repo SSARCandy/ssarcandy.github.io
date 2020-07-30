93ecf40de76351cdc689dcb6dafdb8d4cc81f116
bot <ssarcandy@gmail.com>
2020-07-31 00:51:37 +0800

    Site updated: 2020-07-31 00:51:37

--- sw.js
+++ sw.js
@@ -24,13 +24,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2020/04/22/Build-a-High-Performance-Computing-Cluster-on-GCP/');
-
   precacheUrls.push('/2017/06/26/Coherent-Line-Drawing/');
 
   precacheUrls.push('/2017/04/16/High-Dynamic-Range-Imaging/');
 
   precacheUrls.push('/2016/12/25/MSAuto/');
+
+  precacheUrls.push('/2020/04/22/Build-a-High-Performance-Computing-Cluster-on-GCP/');
 
   precacheUrls.push('/2019/06/11/Monitoring-from-Handcraft-to-Prometheus/');
 
