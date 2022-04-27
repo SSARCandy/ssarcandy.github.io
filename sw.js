736300580e9b258f72d2490287c542f7d14a272a
bot <ssarcandy@gmail.com>
2022-04-28 03:16:22 +0800

    Site updated: 2022-04-28 03:16:21

--- sw.js
+++ sw.js
@@ -28,9 +28,9 @@ var precacheUrls = [];
 
   precacheUrls.push('/2017/06/26/Coherent-Line-Drawing/');
 
-  precacheUrls.push('/2017/04/16/High-Dynamic-Range-Imaging/');
-
   precacheUrls.push('/2016/12/25/MSAuto/');
+
+  precacheUrls.push('/2017/04/16/High-Dynamic-Range-Imaging/');
 
   precacheUrls.push('/2019/06/11/Monitoring-from-Handcraft-to-Prometheus/');
 
